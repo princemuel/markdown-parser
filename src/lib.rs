225853daa45280e607b12115c2267b64b4f72590
Prince Muel <68931805+princemuel@users.noreply.github.com>
2024-09-16 22:31:59 +0100

    feat: add wee alloc and browser console utils
    Signed-off-by: Prince Muel <68931805+princemuel@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,30 @@
-extern crate wasm_bindgen;
+mod parser;
+mod utils;
 
+extern crate cfg_if;
+extern crate wasm_bindgen;
+extern crate web_sys;
+
+// use crate::parser::*;
+use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+cfg_if! {
+    if #[cfg(feature = "wee_alloc")] {
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
 }
 
+// A macro target_scale provide `log!(..)`-style syntax for `console.log` logging.
+// macro_rules! log {
+//     ( $( $t:tt )* ) => {
+//         web_sys::console::log_1(&format!( $( $t )* ).into());
+//     }
+// }
+
 #[wasm_bindgen]
-pub fn run_alert(item: &str) {}
+pub fn parse(input: &str) -> String {
+    utils::set_panic_hook();
+    parser::parse(input.to_string())
+}
